8a99ef2ae787b9c14cd12b6aaca46f5056a9302d
Max Kohl <60585666+Maxkohl@users.noreply.github.com>
2020-05-19 16:00:58 -0400

    Refactored getChartData function to make API call to covid API

--- src/App.js
+++ src/App.js
@@ -15,33 +15,54 @@ class App extends Component {
   }
 
   getChartData() {
-    //insert API ajax call here after testing with dummy data
-    this.setState({
-      chartData: {
-        labels: ["January", "February", "March", "April", "May"],
-        datasets: [
-          {
-            label: "Rainfall",
-            backgroundColor: [
-              "#B21F00",
-              "#C9DE00",
-              "#2FDE00",
-              "#00A6B4",
-              "#6800B4",
-            ],
-            hoverBackgroundColor: [
-              "#501800",
-              "#4B5000",
-              "#175000",
-              "#003350",
-              "#35014F",
-            ],
-            data: [65, 59, 80, 81, 56],
-          },
-        ],
-      },
-    });
+    fetch("https://api.covid19api.com/summary")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            chartData: result,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
   }
+
+  // getChartData() {
+  //   //insert API ajax call here after testing with dummy data
+  //   this.setState({
+  //     chartData: {
+  //       labels: ["January", "February", "March", "April", "May"],
+  //       datasets: [
+  //         {
+  //           label: "Rainfall",
+  //           backgroundColor: [
+  //             "#B21F00",
+  //             "#C9DE00",
+  //             "#2FDE00",
+  //             "#00A6B4",
+  //             "#6800B4",
+  //           ],
+  //           hoverBackgroundColor: [
+  //             "#501800",
+  //             "#4B5000",
+  //             "#175000",
+  //             "#003350",
+  //             "#35014F",
+  //           ],
+  //           data: [65, 59, 80, 81, 56],
+  //         },
+  //       ],
+  //     },
+  //   });
+  // }
 
   render() {
     return (
