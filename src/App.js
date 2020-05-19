eecc4b632f286814243ebb0eebc3fc9777c1b67f
Max Kohl <60585666+Maxkohl@users.noreply.github.com>
2020-05-19 15:46:26 -0400

    Moved dummy chart data out of Chart.js and into getChartData function in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import Chart from "./components/Chart";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Chart />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      chartData: {},
+    };
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    //insert API ajax call here after testing with dummy data
+    this.setState({
+      chartData: {
+        labels: ["January", "February", "March", "April", "May"],
+        datasets: [
+          {
+            label: "Rainfall",
+            backgroundColor: [
+              "#B21F00",
+              "#C9DE00",
+              "#2FDE00",
+              "#00A6B4",
+              "#6800B4",
+            ],
+            hoverBackgroundColor: [
+              "#501800",
+              "#4B5000",
+              "#175000",
+              "#003350",
+              "#35014F",
+            ],
+            data: [65, 59, 80, 81, 56],
+          },
+        ],
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Chart chartData={this.state.chartData} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,27 +5,7 @@ class Chart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      labels: ["January", "February", "March", "April", "May"],
-      datasets: [
-        {
-          label: "Rainfall",
-          backgroundColor: [
-            "#B21F00",
-            "#C9DE00",
-            "#2FDE00",
-            "#00A6B4",
-            "#6800B4",
-          ],
-          hoverBackgroundColor: [
-            "#501800",
-            "#4B5000",
-            "#175000",
-            "#003350",
-            "#35014F",
-          ],
-          data: [65, 59, 80, 81, 56],
-        },
-      ],
+      chartData: props.chartData,
     };
   }
 
@@ -33,7 +13,7 @@ class Chart extends Component {
     return (
       <div>
         <Pie
-          data={this.state}
+          data={this.state.chartData}
           options={{
             title: {
               display: true,
