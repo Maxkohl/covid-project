53b7a986fa2c82ba01b04cd3cced88f3d8edd40d
Max Kohl <60585666+Maxkohl@users.noreply.github.com>
2020-05-20 18:08:49 -0400

    Removed unused imports from App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
 import React, { Component } from "react";
-import PieChart from "./components/PieChart";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import { makeStyles } from "@material-ui/core/styles";
 import Sidebar from "./components/Sidebar";
 import "./App.css";
-
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
-
-const classes = useStyles;
 
 class App extends Component {
   constructor(props) {
